761eab022f44695f285fce012837100c09b3e85b
stageleft <sugano-github@elder-alliance.org>
2023-01-16 18:24:08 +0900

    add trim-and-mirror function

--- convert.rb
+++ convert.rb
@@ -16,10 +16,21 @@ def trim(in_name, out_name, out_x, out_y, out_w, out_h)
     in_image  = Cairo::ImageSurface.from_png(in_name)
     out_image = Cairo::ImageSurface.new(out_w, out_h)
     out_canvas = Cairo::Context.new(out_image)
+    out_canvas.set_source(in_image, 0-out_x, 0-out_y)
     out_canvas.fill do
+        out_canvas.rectangle(0, 0, out_w, out_h)
+    end
+    out_image.write_to_png(out_name)
+end
+def trim_and_mirror(in_name, out_name, out_x, out_y, out_w, out_h)
+    in_image  = Cairo::ImageSurface.from_png(in_name)
+    out_image = Cairo::ImageSurface.new(out_w, out_h)
+    out_canvas = Cairo::Context.new(out_image)
+    out_canvas.fill do
+        mirror_out_canvas = Cairo::Matrix.new(-1, 0, 0, 1, out_w, 0)
+        out_canvas.set_matrix(mirror_out_canvas)
         out_canvas.set_source(in_image, 0-out_x, 0-out_y)
         out_canvas.rectangle(0, 0, out_w, out_h)
-        out_canvas.fill();
     end
     out_image.write_to_png(out_name)
 end
@@ -36,6 +47,7 @@ out_dir = ARGV[1]
 Dir.glob("#{in_dir}/*").each{|in_name|
     out_name = in_name.gsub("#{in_dir}", "#{out_dir}")
     trim(in_name, out_name, out_xpos, out_ypos, out_width, out_height)
+    trim_and_mirror(in_name, out_name.gsub(".PNG", "r.PNG"), out_xpos, out_ypos, out_width, out_height)
 }
 
 exit 0
